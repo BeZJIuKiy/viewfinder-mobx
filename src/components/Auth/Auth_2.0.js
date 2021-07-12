6ee0517a9d16e8d189407b1e66c520ef4064e249
BeZJIuKiy <matveenkov.sasha@gmail.com>
2021-07-12 13:39:40 +0300

    auth 2.0 is added

--- src/components/Auth/Auth_2.0.js
+++ src/components/Auth/Auth_2.0.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/core/styles";
-import backgroundImage from "./images/backgroundChanged02.jpg"
+// import backgroundImage from "./images/backgroundChanged02.jpg"
+import backgroundImage from "./images/backgroundNew.jpg"
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
@@ -20,12 +21,12 @@ const useStyles = makeStyles((theme) => ({
         position: "relative",
     },
 
-    mainSubstrate: {
+    mainSubstrateLeft: {
         width: "50%",
         height: "100%",
 
         backgroundColor: "#333",
-        opacity: 0.4,
+        opacity: 0.7,
 
         position: "absolute",
         top: 0,
@@ -33,11 +34,24 @@ const useStyles = makeStyles((theme) => ({
         zIndex: 1,
     },
 
+    mainSubstrateRight: {
+        width: "50%",
+        height: "100%",
+
+        backgroundColor: "#333",
+        opacity: 0.2,
+
+        position: "absolute",
+        top: 0,
+        right: 0,
+        zIndex: 1,
+    },
+
     mainTitle: {
         color: "white",
-        fontSize: "7vw",
+        fontSize: "4vw",
         fontWeight: 500,
-        marginBottom: "8vw",
+        // marginBottom: "1vw",
     },
 
     mainItem: {
@@ -47,7 +61,7 @@ const useStyles = makeStyles((theme) => ({
         position: "absolute",
         top: "50%",
         left: "25%",
-        transform: "translate(-50%, -70%)",
+        transform: "translate(-50%, -25%)",
         zIndex: 2,
     },
 
@@ -79,7 +93,7 @@ const useStyles = makeStyles((theme) => ({
     },
 
     mainFormBorder: {
-        width: "40%",
+        width: "50%",
 
         // backgroundColor: "#444",
         // opacity: 0.7,
@@ -92,45 +106,53 @@ const useStyles = makeStyles((theme) => ({
 
     mainFormTitle: {
         fontSize: "2.5vw",
-        textAlign: "right",
+        textAlign: "left",
     },
 
     mainFormText: {
-        margin: "10%",
+        marginBottom: "6%",
         fontSize: "1.2vw",
     },
 
     mainFormBtn: {
         marginTop: 10,
+        display: "flex",
+        justifyContent: "space-around",
     },
-}));
+}))
 
 export const Auth_2_0 = () => {
     const classes = useStyles();
 
     return (
         <div className={classes.main}>
-            <div className={classes.mainSubstrate}></div>
+            <div className={classes.mainSubstrateLeft}></div>
             <div className={classes.mainItem}>
                 <div className={classes.mainTitle}>ViewFinder</div>
                 <div className={classes.mainForm}>
                     {/*<div className={classes.mainFormBorderSubstrate}></div>*/}
                     <div className={classes.mainFormBorder}>
-                        <div className={classes.mainFormTitle}>ViewFinder</div>
+                        {/*<div className={classes.mainFormTitle}>ViewFinder</div>*/}
                         <div className={classes.mainFormText}>текст текст текст много текста</div>
                         <div className={classes.mainFormBtn}>
                             <Button variant="contained" color="secondary" style={{fontSize: "1.5vw", width: "10vw"}}>
                                 DEMO
                             </Button>
-                        </div>
-                        <div className={classes.mainFormBtn}>
+
                             <Button variant="contained" color="primary" style={{fontSize: "1.5vw", width: "10vw"}}>
-                                Login in
+                                Log in
                             </Button>
                         </div>
+                        {/*<div className={classes.mainFormBtn}>*/}
+                        {/*    <Button variant="contained" color="primary" style={{fontSize: "1.5vw", width: "10vw"}}>*/}
+                        {/*        Login in*/}
+                        {/*    </Button>*/}
+                        {/*</div>*/}
                     </div>
                 </div>
             </div>
+
+            <div className={classes.mainSubstrateRight}></div>
             <div className={classes.mainItem}></div>
         </div>
     );
